5222e90f3c4f94868a7ffb764abe23f7bcf9918b
rajesh1906 <rajeshk.chelluri@gmail.com>
2017-01-21 15:17:22 +0530

    committed 21 jan

--- app/src/main/java/com/metrial/chrajeshkumar/newspaper/Helper/HandlingViews.java
+++ app/src/main/java/com/metrial/chrajeshkumar/newspaper/Helper/HandlingViews.java
@@ -4,8 +4,8 @@ package com.metrial.chrajeshkumar.newspaper.Helper;
  * Created by ChRajeshKumar on 9/1/2016.
  */
 public interface HandlingViews {
-    public void implementation(String message,int position);
-    public void conncetion(String value,String title,String description);
-    public int getContantField();
-    public void dialog_control();
+     void implementation(String message,int position);
+     void conncetion(String value,String title,String description);
+     int getContantField();
+     void dialog_control();
 }
